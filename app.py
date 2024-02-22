4acd4f32386731f448a33a7f0b68e1c558023e3b
joseg <joseg@hpe.com>
2024-02-22 16:31:31 +0100

    small update

--- app.py
+++ app.py
@@ -6,13 +6,6 @@ from src import guardrails as guardrails
 logging.basicConfig(level=logging.DEBUG)
 rails = guardrails.initialize_guardrails(verbose=True)
 
-info = rails.explain()
-print(info.print_llm_calls_summary())
-
-for call in range(len(info.llm_calls)):
-    print(info.llm_calls[call].prompt)
-    print(info.llm_calls[call].completion)
-
 
 @cl.on_message  # called every time a user sends a message
 async def main(message: cl.Message):
@@ -22,13 +15,6 @@ async def main(message: cl.Message):
     message_history.append({"role": "user", "content": message.content})
 
     bot_message = rails.generate(messages=message_history)
-
-    info = rails.explain()
-    print(info.print_llm_calls_summary())
-
-    for call in range(len(info.llm_calls)):
-        print(info.llm_calls[call].prompt)
-        print(info.llm_calls[call].completion)
 
     # Send bot message
     msg = cl.Message(content=bot_message["content"])
