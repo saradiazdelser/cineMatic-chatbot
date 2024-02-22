f8cbe968e186d56e5201245eebe4034b79daf0b2
joseg <joseg@hpe.com>
2024-02-22 08:57:51 +0100

    before merge

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ import logging
 
 import chainlit as cl
 
-import asyncio
 
 from src import guardrails as guardrails
 
 
 logging.basicConfig(level=logging.DEBUG)
 
-rails, streaming_handler = guardrails.initialize_guardrails(verbose=False)
+rails = guardrails.initialize_guardrails(verbose=True)
+
 
 @cl.on_chat_start
 def start_chat():
@@ -22,37 +22,11 @@ def start_chat():
 @cl.on_message  # called every time a user sends a message
 async def main(message: cl.Message):
     """Receive a user message and return a bot message"""
-    
-    message_history = cl.user_session.get("message_history")    
+
+    message_history = cl.user_session.get("message_history")
     message_history.append({"role": "user", "content": message.content})
-    
-    if streaming_handler:
 
-        msg = cl.Message(content="")
-        await msg.send()
-        
-        async def process_tokens():
-            async for chunk in streaming_handler:
-                logging.debug(f"CHUNK: {chunk}")
-                # Or do something else with the token
-                await msg.stream_token(chunk)
-
-        asyncio.create_task(process_tokens())
-        
-        message_history.append({"role": "assistant", "content": msg.content})
-        await msg.update()
-        
-        bot_message = await rails.generate_async(
-            messages=message_history, streaming_handler=streaming_handler
-        )
-
-        msg = cl.Message(content=bot_message["content"])
-        await msg.send()
-
-        
-
-    else:
-        bot_message = rails.generate(messages=message_history)
-        # Send bot message
-        msg = cl.Message(content=bot_message["content"])
-        await msg.send()
+    bot_message = rails.generate(messages=message_history)
+    # Send bot message
+    msg = cl.Message(content=bot_message["content"])
+    await msg.send()
--- src/guardrails.py
+++ src/guardrails.py
@@ -5,29 +5,23 @@ from nemoguardrails.streaming import StreamingHandler
 from pathlib import Path
 
 
-def initialize_guardrails(path_to_config:Path=Path().cwd() / "src" / "config", verbose:bool=False)-> LLMRails:
+def initialize_guardrails(
+    path_to_config: Path = Path().cwd() / "src" / "config", verbose: bool = True
+) -> LLMRails:
     """Load configuration and initialize rails"""
     try:
         if not path_to_config.exists():
-            raise(ValueError(f"Path_to_config: {path_to_config} does NOT exist"))
+            raise (ValueError(f"Path_to_config: {path_to_config} does NOT exist"))
     except Exception as e:
         logging.error(e)
         exit(0)
 
     config = RailsConfig.from_path(str(path_to_config))
-    
-    streaming_handler = None
-    
-    try:
-        if config.streaming:
-            streaming_handler = StreamingHandler()    
-    
-    except:
-        logging.info("No streaming")    
-    
+
     rails = LLMRails(config, verbose=verbose)
+
     # test
-    # _ = rails.generate(messages=[{"role": "user", "content": "Hi there!"}])
-    logging.info('Successfully intialized guardrails')
-    
-    return rails, streaming_handler
+    _ = rails.generate(messages=[{"role": "user", "content": "Hi there!"}])
+    logging.info("Successfully intialized guardrails")
+
+    return rails
