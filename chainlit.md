a35cee97d70a5f164581af6a7d9a4ab6a0dbae83
Sara Díaz <sara.diaz.delser@gmail.com>
2024-07-08 15:03:38 +0200

    frontend

--- chainlit.md
+++ chainlit.md
@@ -1,1 +1,8 @@
 # Welcome to our Guardrails Demo
+The scope of this project is to showcase the basic functionalities of [NeMo Guardrails](https://github.com/NVIDIA/NeMo-Guardrails/tree/main). The rails are programmed to make the LLM stay on topic: the bot will only answer movie-related questions.
+
+The deployed model is the `mistralai/Mixtral-8x7B-Instruct-v0.1` model.
+
+> **Note**
+> Guardrails technologies are not perfect and are still on refinement. Unexpected or inaccurate responses may occur.
+
--- src/frontend.py
+++ src/frontend.py
@@ -6,6 +6,22 @@ from src import guardrails as guardrails
 from src.guardrails import LLMRails
 
 
+@cl.set_chat_profiles
+async def chat_profile():
+    return [
+        cl.ChatProfile(
+            name="Moderated",
+            markdown_description="Chatbot is moderated using the NVIDIA **NeMo Guardrails** framework.",
+            icon="public/moderated_icon.jpg",
+        ),
+        cl.ChatProfile(
+            name="Unmoderated",
+            markdown_description="Chatbot uses no moderation framework. Questions go directly to the model.",
+            icon="public/unmoderated_icon.jpg",
+        ),
+    ]
+
+
 @cl.on_chat_start
 async def on_chat_start():
     rails: LLMRails = guardrails.initialize_guardrails()
@@ -17,15 +33,20 @@ async def on_chat_start():
 async def main(message: cl.Message):
     """Receive a user message and return a bot message"""
     # Retrieve user variables
+    chat_profile = cl.user_session.get("chat_profile")
     chat_history: List = cl.user_session.get("chat_history")
     rails: LLMRails = cl.user_session.get("rails")
     last_user_message = {"role": "user", "content": message.content or ""}
 
-    # Generate a bot message
-    new_chat_history, bot_message = await guardrails.generate_message(
-        last_user_message, rails, chat_history
-    )
+    if chat_profile == "Moderated":
+        # Generate a bot message
+        new_chat_history, bot_message = await guardrails.generate_message(
+            last_user_message, rails, chat_history
+        )
 
+    else:
+        bot_message = {"role": "bot", "content": "Unmoderated chatbot response"}
+        new_chat_history = chat_history
     cl.user_session.set("chat_history", new_chat_history)
 
     await cl.Message(content=bot_message["content"]).send()
