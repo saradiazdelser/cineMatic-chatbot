940de5cfab1a0c9ba404d4fa022c5d47bdaa192b
Sara Díaz <sara.diaz.delser@gmail.com>
2024-07-11 13:58:07 +0200

    local deployment

--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,7 @@
 INFERENCE_ENDPOINT = "http://10.10.78.11:8081"
 INFERENCE_HEALTH_ENDPOINT = "http://10.10.78.11:8081/health"
-RETRIEVAL_ENDPOINT = "http://rag:6000/search"
-ALIGNSCORE_ENDPOINT = "http://alignscore:5000/base_model"
+# RETRIEVAL_ENDPOINT = "http://rag:6000/search" # Access RAG in Docker network
+RETRIEVAL_ENDPOINT = "http://localhost:6000/search"
+# ALIGNSCORE_ENDPOINT = "http://alignscore:5000/base_model" # Access AlignScore in Docker network
+ALIGNSCORE_ENDPOINT = "http://localhost:5000/base_model"
 FACTCHECKING = False
