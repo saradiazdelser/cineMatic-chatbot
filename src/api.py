bfa51be3e93d7a9b88c397b544a4c99e21da864e
Sara Díaz <sara.diaz.delser@gmail.com>
2024-07-11 13:57:30 +0200

    handle history

--- src/api.py
+++ src/api.py
@@ -8,13 +8,13 @@ from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 
-from src.chat import Chat
+from src.chat import ChatBot
 from src.health import health_check
 
 app = FastAPI()
 logger = logging.getLogger(__name__)
 
-chat = Chat()
+chat: ChatBot = ChatBot()
 
 
 class Message(BaseModel):
@@ -26,31 +26,43 @@ class Message(BaseModel):
 router_api = APIRouter()
 
 
-@router_api.post("/generate_moderated")
-async def generate(messages: List[Message]):
-    """Receive a list of user messages and return a bot message using the moderated chatbot"""
-    logger.info(f"API:: Received messages: {messages}")
-    last_user_message = messages[-1].model_dump()
-    messages = [msg.model_dump() for msg in messages[:-1] if msg]
-    _, bot_message = await chat.generate_moderated_message(last_user_message, messages)
-    return bot_message
+@router_api.post("/generate_moderated", tags=["Chatbot"])
+async def generate_moderated(message: Message) -> Message:
+    """Receive a user message and return a bot message using the moderated chatbot"""
+    logger.info(f"API :: Received message: {message}")
+    user_message = message.model_dump()
+    bot_message = await chat.generate_moderated_message(user_message)
+    return Message(**bot_message)
 
 
-@router_api.post("/generate_unmoderated")
-async def generate(messages: List[Message]):
+@router_api.post("/generate_unmoderated", tags=["Chatbot"])
+async def generate_unmoderated(message: Message):
     """Receive a list of user messages and return a bot message using the unmoderated chatbot"""
-    logger.info(f"API:: Received messages: {messages}")
-    last_user_message = messages[-1].model_dump()
-    messages = [msg.model_dump() for msg in messages[:-1] if msg]
-    _, bot_message = await chat.generate_unmoderated_message(
-        last_user_message, messages
-    )
-    return bot_message
+    logger.info(f"API :: Received message: {message}")
+    user_message = message.model_dump()
+    bot_message = await chat.generate_unmoderated_message(user_message)
+    return Message(**bot_message)
 
 
-@router_api.get("/health")
-async def health():
+@router_api.get("/history", tags=["Conversation History"])
+def history() -> List:
+    return chat.get_history()
+
+
+@router_api.post("/clear", tags=["Conversation History"])
+def clear_history():
+    chat.clear_history()
+    return {"status": "success"}
+
+
+@router_api.get("/healthz", tags=["Health"])
+async def healthz():
     return health_check()
+
+
+@router_api.get("/health", tags=["Health"])
+async def health():
+    return health_check(details=True)
 
 
 # Use public endpoint for static files
--- src/chat.py
+++ src/chat.py
@@ -1,6 +1,7 @@
 import logging
+from collections import deque
 from pathlib import Path
-from typing import Dict, List, Tuple
+from typing import Deque, Dict, List, Tuple
 
 from huggingface_hub import InferenceClient
 from jinja2 import Environment, Template
@@ -12,14 +13,28 @@ from src.settings import INFERENCE_ENDPOINT
 logger = logging.getLogger(__name__)
 
 
-class Chat:
-    def __init__(self):
+class ChatBot:
+    def __init__(self, memory_size: int = 10):
         self.rails: LLMRails = None
         self.client: InferenceClient = None
         self.prompt_template: Template = None
         self.system_prompt: str = None
+        self.history: Deque[Dict] = deque(maxlen=memory_size)
         self.initialize_guardrails()
         self.initialize_client()
+
+    def __str__(self) -> str:
+        return (
+            "ChatBot(rails={}, client={}, prompt_template={}, system_prompt={})".format(
+                str(self.rails),
+                str(self.client),
+                str(self.prompt_template),
+                str(self.system_prompt),
+            )
+        )
+
+    def __repr__(self) -> str:
+        return self.__str__()
 
     @staticmethod
     def post_processing(text: str) -> str:
@@ -28,6 +43,19 @@ class Chat:
         # Decode the bytes back to a UTF-8 string
         cleaned_text = utf8_encoded_text.decode("utf-8")
         return cleaned_text
+
+    def clear_history(self):
+        """Clear conversation history."""
+        self.history.clear()
+        logger.info("Conversation history cleared.")
+
+    def get_history(self) -> List[Dict]:
+        """Get current conversation history."""
+        return list(self.history)
+
+    def add_history(self, message: Dict[str, str]):
+        """Add message to conversation history."""
+        self.history.append(message)
 
     def build_prompt_from_config(self, config: RailsConfig):
         """Build prompt template from RailsConfig object."""
@@ -53,12 +81,12 @@ class Chat:
         self.prompt_template = env.from_string(generate_bot_message)
         return
 
-    def initialize_client(self) -> InferenceClient:
+    def initialize_client(self):
         self.client = InferenceClient(model=INFERENCE_ENDPOINT)
 
     def initialize_guardrails(
         self, path_to_config: Path = Path().cwd() / "src" / "config"
-    ) -> LLMRails:
+    ):
         """Load configuration and initialize rails"""
         try:
             if not path_to_config.exists():
@@ -79,21 +107,20 @@ class Chat:
         self.rails.register_action_param("rag_prompt", config.custom_data["rag_prompt"])
 
         logger.info("Successfully initialized guardrails")
-        return self.rails
+        return
 
     async def generate_moderated_message(
-        self, user_message: Dict[str, str], chat_history: List[Dict[str, str]] = []
-    ) -> Tuple[List[Dict[str, str]], Dict[str, str]]:
+        self, user_message: Dict[str, str]
+    ) -> Dict[str, str]:
         """Generate a bot message based on the user message using the NeMo Guardrails framework for moderation.
         Args:
             user_message (Dict[str, str]): User message
-            chat_history (List[Dict[str, str]], optional): Chat history. Defaults to [].
         Returns:
-            List[Dict[str, str]]: Updated chat history
             Dict[str, str]: Bot message
         """
         # Save user message to history
-        chat_history.append(user_message)
+        self.add_history(user_message)
+        chat_history = self.get_history()
 
         # Generate bot message
         self.rails.register_action_param("chat_history", chat_history)
@@ -101,33 +128,29 @@ class Chat:
             messages=chat_history, return_context=True
         )
         bot_message = response[0]
-
         bot_message["content"] = self.post_processing(bot_message["content"])
 
         # Save bot message to history
-        chat_history.append(bot_message)
+        self.add_history(bot_message)
 
-        # clean
-        return chat_history, bot_message
+        return bot_message
 
     async def generate_unmoderated_message(
         self,
         user_message: Dict[str, str],
-        chat_history: List[Dict[str, str]] = [],
-    ) -> Tuple[List[Dict[str, str]], Dict[str, str]]:
+    ) -> Dict[str, str]:
         """Generate a bot message based on the user message using the unmoderated chatbot.
         Args:
             user_message (Dict[str, str]): User message
-            chat_history (List[Dict[str, str]], optional): Chat history. Defaults to [].
         Returns:
-            List[Dict[str, str]]: Updated chat history
             Dict[str, str]: Bot message
         """
         # Save user message to history
-        chat_history.append(user_message)
+        self.add_history(user_message)
+        chat_history = self.get_history()
 
         # Get RAG
-        action_result = await retrieve_information()
+        action_result = await retrieve_information(chat_history=chat_history)
         relevant_context: str = action_result.return_value
 
         # Generate bot message
@@ -136,13 +159,13 @@ class Chat:
             relevant_context=relevant_context,
             history=format_chat_history(chat_history),
         )
-        response = await self.client.text_generation(prompt=prompt)
-        response = self.post_processing(response)
 
+        logger.info(f"Prompt :: {prompt}")
+        response = self.client.text_generation(prompt=prompt, max_new_tokens=100)
+        response = self.post_processing(response)
         bot_message = {"role": "bot", "content": response}
 
         # Save bot message to history
-        chat_history.append(bot_message)
+        self.add_history(bot_message)
 
-        # clean
-        return chat_history, bot_message
+        return bot_message
